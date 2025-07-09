d59a406b39f401f3f49993256ddd6271198a1020
Genyus <genyus@gmail.com>
2025-07-09 01:30:47 +0100

    refactor: simplify App implementation

--- src/main/java/com/cbfacademy/App.java
+++ src/main/java/com/cbfacademy/App.java
@@ -3,23 +3,15 @@ package com.cbfacademy;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class App {
     public static void main(String[] args) {
-        try {
-            URL url = new URI("https://codingblackfemales.com").toURL();
-            URLConnection connection = url.openConnection();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new URI("https://codingblackfemales.com").toURL().openStream()))) {
+            String inputLine;
 
-            connection.connect();
-            // Using try-with-resources to ensure resources are closed
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String inputLine;
-
-                while ((inputLine = reader.readLine()) != null) {
-                    System.out.println(inputLine);
-                }
+            while ((inputLine = reader.readLine()) != null) {
+                System.out.println(inputLine);
             }
         } catch (Exception e) {
             e.printStackTrace();
